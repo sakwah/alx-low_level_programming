3595cd4821ea64101d06ea5217a605682562c42f
Michael Sakwah <msakwahm@gmail.com>
2022-04-13 22:56:34 +0000

    Nature made the natural numbers; All else is the work of women

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,5 +1,5 @@
-#include "main"
-#include <stdio>
+#include "main.h"
+#include <stdio.h>
 
 /**
  * main - computes and prints the sum of all the multiples of 3 or 5 below 1024
@@ -19,4 +19,5 @@ int main(void)
 		}
 	}
 	printf("%lu", sum);
+	return (0);
 }
