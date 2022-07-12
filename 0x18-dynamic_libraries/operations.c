dbc669689a75023910006c2e96ec6ea8072d8f59
sakwah <msakwahm@gmail.com>
2022-07-12 00:34:05 +0000

    Let's call C functions from Python

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,31 @@
+#include "operations.h"
+int add(int a, int b)
+{
+	int c;
+	c = a + b;
+	return (c);
+}
+int sub(int a, int b)
+{
+	int c;
+	c = a - b;
+	return (c);
+}
+int mul(int a, int b)
+{
+	int c;
+	c = a * b;
+	return (c);
+}
+int div(int a, int b)
+{
+	int c;
+	c = a / b;
+	return (c);
+}
+int mod(int a, int b)
+{
+	int c;
+	c = a % b;
+	return (c);
+}
