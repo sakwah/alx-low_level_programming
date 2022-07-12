43a447e7b6c2bac4202ca0a2141c4a88830fa58e
sakwah <msakwahm@gmail.com>
2022-07-12 15:43:23 +0000

    Delete at index

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,6 +1,28 @@
 #include "lists.h"
 
 /**
+ * dlistint_len - returns the number of nodes in a doubly linked list
+ * @h: pointer to the list
+ *
+ * Return: number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t nodes = 0;
+
+	if (!h)
+		return (0);
+
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
+}
+
+/**
  * delete_dnodeint_at_index - deletes node at givn index of doubly linked list
  * @head: pointer to the list
  * @index: index of the node to delete
